3b34aaff7804324048e0e1a48aa8f9054c15fefb
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-29 09:34:01 -0300

    aula #05 - modulos (ngModule)

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CursosModule } from './cursos/cursos.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponente } from './meu-primeiro-componente/meu-primeiro-componente.component';
 import { MeuSegundoComponentComponent } from './meu-segundo-component/meu-segundo-component.component';
 
 @NgModule({
-  declarations: [AppComponent, MeuPrimeiroComponente, MeuSegundoComponentComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponente,
+    MeuSegundoComponentComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
