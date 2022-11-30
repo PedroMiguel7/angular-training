29e2ababa24d57e54c52283429059f35b22223cc
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-30 08:27:38 -0300

    aula #09 - Property Binding e Interpolação

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,6 +9,8 @@ export class DataBindingComponent {
   url: string = 'https://github.com/PedroMiguel7';
 
   cursolAngular: Boolean = true;
+  urlImage: string =
+    'https://media.tenor.com/XG8WXd4R7RYAAAAC/pato-caminando.gif';
 
   getValor() {
     return 1;
