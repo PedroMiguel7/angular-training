d428b91d823a1b536f15a660bd5a31e7dac406a1
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-11-30 09:25:37 -0300

    aula #10 - Class e Style binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -17,4 +17,4 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
