158e5415bac1739a56532a11cf4a32ab51185676
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-06 08:57:44 -0300

    Aula #25 - Diretivas: ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.scss']
+  styleUrls: ['./diretiva-ngif.component.scss'],
 })
 export class DiretivaNgifComponent {
+  cursos: string[] = ['Angular'];
 
+  mostrarCursos = false;
+
+  BrincksMinha = 'mostrar ';
+
+  OnmostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+    this.BrincksMinha = this.mostrarCursos ? 'esconder ' : 'mostrar ';
+  }
 }
