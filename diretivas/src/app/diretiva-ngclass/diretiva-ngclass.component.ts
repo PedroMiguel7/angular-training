7d4d21c11bb964acc0c659d01010baf293726ce2
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-07 08:38:56 -0300

    Aula #29 - Diretivas: ngClass

--- diretivas/src/app/diretiva-ngclass/diretiva-ngclass.component.ts
+++ diretivas/src/app/diretiva-ngclass/diretiva-ngclass.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngclass',
   templateUrl: './diretiva-ngclass.component.html',
-  styleUrls: ['./diretiva-ngclass.component.scss']
+  styleUrls: ['./diretiva-ngclass.component.scss'],
 })
 export class DiretivaNgclassComponent {
+  meuFavorito = false;
 
+  onClick() {
+    this.meuFavorito = !this.meuFavorito;
+  }
 }
