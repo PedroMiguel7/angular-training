6481c95e7bdb4f0dc3e7060b1b74b40043dfc990
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-26 14:07:23 -0300

    Aula #53 - Rotas: definindo e extraindo parametros de roteamento

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -14,11 +14,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 registerLocaleData(pt);
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent, CursoDetalheComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
