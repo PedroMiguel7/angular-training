24397e34528a017a698167b22fb36962516850e4
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-27 09:54:58 -0300

    Aula #54 - Rotas: Escutando mudanças nos parametros de roteamento

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -8,11 +9,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CursoDetalheComponent implements OnInit {
   id: any;
+  inscricao: Subscription | undefined;
 
   constructor(private route: ActivatedRoute) {
-    console.log(this.route);
-    this.id = this.route.snapshot.params['id'];
+    // this.id = this.route.snapshot.params['id'];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao?.unsubscribe();
+  }
 }
