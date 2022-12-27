01e96e090b1d7fbebb7cf863fdd1aae98cdf1cc5
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-27 12:00:04 -0300

    Aula #55 - Rotas Imperativas: Redirecionamento via código

--- rotas/src/app/cursos/cursos.service.ts
+++ rotas/src/app/cursos/cursos.service.ts
@@ -7,20 +7,19 @@ export class CursosService {
   getCursos() {
     return [
       { id: 1, nome: 'Angular 2' },
-      { id: 2, nome: 'Java' }
+      { id: 2, nome: 'Java' },
     ];
   }
 
-  getCurso(id: number): any {
+  getCurso(id: number) {
     let cursos = this.getCursos();
     for (let i = 0; i < cursos.length; i++) {
       let curso = cursos[i];
-      if (curso.id === id) {
+      if (curso.id == id) {
         return curso;
-      } else {
-        return null;
       }
     }
+    return null;
   }
 
   constructor() {}
