bad9eec7a3158a23846c90c18022347ae45e53d9
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-28 11:21:59 -0300

    Aula #60 - Rotas Filhas

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import pt from '@angular/common/locales/pt';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosModule } from './cursos/cursos.module';
 
 registerLocaleData(pt);
 
@@ -22,6 +24,7 @@ registerLocaleData(pt);
     HttpClientModule,
     BrowserAnimationsModule,
     CursosModule,
+    AlunosModule,
     AppRoutingModule,
   ],
   // providers: [],
