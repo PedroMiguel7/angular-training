c7affca2b4b9ea013d6ffd243c610b7a6865db2a
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-29 16:49:52 -0300

    Aula #83 - Forms (template driven):Refatorando CSS e mensagem de erro

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -22,4 +22,14 @@ export class TemplateFormComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  verificaValidTouched(campo: any) {
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo: any) {
+    return {
+      'is-invalid': this.verificaValidTouched(campo),
+    };
+  }
 }
