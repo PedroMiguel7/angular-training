fc2d72201ae56fc9c8f69d59fe1fed7902074775
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-29 15:02:52 -0300

    Aula #74 - Forms (template driven): Controles ngForm, ngSubmit e ngModel

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +8,9 @@ import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataFormComponent,
-    TemplateFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DataFormComponent, TemplateFormComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
