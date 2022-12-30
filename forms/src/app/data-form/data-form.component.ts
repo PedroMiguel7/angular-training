630d9da993b65f06823db79cf96c9145cb4ab745
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-30 18:40:05 -0300

    Aula #108 - Formulário reativos: Validação Customizada (FormArray Checkboxes)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormArray,
   FormBuilder,
   FormControl,
@@ -11,6 +12,7 @@ import { EstadoBr } from '../shared/models/estado-br';
 import { DropdwonService } from '../shared/services/dropdwon.service';
 // import { Observable } from 'rxjs';
 import { ConsultaCepService } from '../shared/services/consulta-cep.service';
+import { FormValidations } from '../shared/services/form-validations';
 
 @Component({
   selector: 'app-data-form',
@@ -75,7 +77,10 @@ export class DataFormComponent implements OnInit {
   buildFrameworks() {
     const values = this.frameworks.map((v) => new FormControl(false));
 
-    return this.formBuilder.array(values);
+    return this.formBuilder.array(
+      values,
+      FormValidations.requiredMinCheckBox(1)
+    );
   }
 
   onSubmit() {
