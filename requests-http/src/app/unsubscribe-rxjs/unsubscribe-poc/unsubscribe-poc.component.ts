9c64f686d010411169a02a888c68445413bccdac
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-31 20:13:37 -0300

    Aula #123 - Http + RxJS (2-parte): Criando componenetes de importando modulos

---
+++ requests-http/src/app/unsubscribe-rxjs/unsubscribe-poc/unsubscribe-poc.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { EnviarValorService } from '../enviar-valor.service';
+
+@Component({
+  selector: 'app-unsubscribe-poc',
+  templateUrl: './unsubscribe-poc.component.html',
+  styleUrls: ['./unsubscribe-poc.component.scss'],
+})
+export class UnsubscribePocComponent implements OnInit {
+  mostrarComponentes = true;
+
+  constructor(private serviceE: EnviarValorService) {}
+
+  ngOnInit() {}
+
+  emitirValor(valor: string) {
+    
+  }
+
+  destruirComponentes() {
+    this.mostrarComponentes = !this.mostrarComponentes;
+  }
+}
--- requests-http/src/app/unsubscribe-rxjs/unsubscribe-rxjs.module.ts
+++ requests-http/src/app/unsubscribe-rxjs/unsubscribe-rxjs.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UnsubscribePocComponent } from './unsubscribe-poc/unsubscribe-poc.component';
+import { PocBaseComponent } from './poc-base/poc-base.component';
+import { UnsubscribeRxjsRoutingModule } from './unsubscribe-rxjs-routing.module';
+import { PocUnsubComponent } from './componentes/poc-unsub.component';
+import { PocComponent } from './componentes/poc.component';
+import { PocAsyncComponent } from './componentes/poc-async.component';
+import { PocTakeUntilComponent } from './componentes/poc-take-until.component';
+import { PocTakeComponent } from './componentes/poc-take.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    UnsubscribePocComponent,
+    PocComponent,
+    PocAsyncComponent,
+    PocTakeUntilComponent,
+    PocTakeComponent,
+    PocUnsubComponent,
+    PocBaseComponent,
+  ],
+  imports: [CommonModule, UnsubscribeRxjsRoutingModule],
 })
-export class UnsubscribeRxjsModule { }
+export class UnsubscribeRxjsModule {}
