9c41416e46414f76961e28f2e5cc2c87498d63c2
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-12-31 19:31:27 -0300

    Aula #120 - Http GET: listar registros

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../curso';
+import { CursosService } from '../cursos.service';
 
 @Component({
   selector: 'app-cursos-lista',
   templateUrl: './cursos-lista.component.html',
-  styleUrls: ['./cursos-lista.component.css']
+  styleUrls: ['./cursos-lista.component.scss'],
+  preserveWhitespaces: true,
 })
 export class CursosListaComponent implements OnInit {
+  cursos?: any;
 
-  constructor() { }
+  constructor(private service: CursosService) {}
 
   ngOnInit() {
+    this.service.list().subscribe((dados: Curso[]) => (this.cursos = dados));
   }
-
 }
--- requests-http/src/app/cursos/cursos.module.ts
+++ requests-http/src/app/cursos/cursos.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosRoutingModule } from './cursos-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CursosListaComponent } from './cursos-lista/cursos-lista.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CursosRoutingModule
-  ]
+  declarations: [CursosListaComponent],
+
+  imports: [CommonModule, CursosRoutingModule, ReactiveFormsModule],
 })
-export class CursosModule { }
+export class CursosModule {}
