6670c3853706b90fb2d6534e186e8b1f640ed1b9
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-01-02 13:49:45 -0300

    Aula #136 - Http: Upload de Arquivo: Back-end com node.js

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const multiparty = require("connect-multiparty");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const multipartMiddleware = multiparty({ uploadDir: "./uploads" });
+
+app.post("/upload", multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+});
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
+app.listen(8000, () => {
+  console.log("Rodando porta 8000");
+});
