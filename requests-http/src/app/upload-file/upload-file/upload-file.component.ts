1c112774afd2820eda6345045607b2265d97ad55
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2023-01-02 13:26:50 -0300

    Aula #135 - Upload de Arquivo: Formulário Upload com bootstrap 5

--- requests-http/src/app/upload-file/upload-file/upload-file.component.ts
+++ requests-http/src/app/upload-file/upload-file/upload-file.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.scss']
+  styleUrls: ['./upload-file.component.scss'],
 })
 export class UploadFileComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onChange(event: any) {
+    const selectedFile = <FileList>event.srcElement.files;
+    console.log(selectedFile);
   }
-
 }
